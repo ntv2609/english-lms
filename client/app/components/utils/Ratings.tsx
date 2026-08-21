012ee71d46d0f6d22b172a0feead15c520b4faaa
ntv2609 <ntv2609@gmail.com>
2026-08-21 20:27:42 +0700

    fix stars array type trong Ratings component

--- client/app/components/utils/Ratings.tsx
+++ client/app/components/utils/Ratings.tsx
@@ -2,15 +2,24 @@ import React, { FC } from "react";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { BsStarHalf } from "react-icons/bs";
 
-interface Props { rating: number; }
+interface Props {
+  rating: number;
+}
 
 const Ratings: FC<Props> = ({ rating }) => {
-  const stars = [];
+  // Thêm kiểu React.ReactNode[] để TypeScript không bắt bẻ mảng rỗng
+  const stars: React.ReactNode[] = [];
+
   for (let i = 1; i <= 5; i++) {
-    if (i <= rating) stars.push(<AiFillStar key={i} size={14} className="text-black dark:text-white mr-0.5" />);
-    else if (i === Math.ceil(rating) && !Number.isInteger(rating)) stars.push(<BsStarHalf key={i} size={13} className="text-black dark:text-white mr-0.5" />);
-    else stars.push(<AiOutlineStar key={i} size={14} className="text-black dark:text-white opacity-30 mr-0.5" />);
+    if (i <= rating) {
+      stars.push(<AiFillStar key={i} size={14} className="text-black dark:text-white mr-0.5" />);
+    } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
+      stars.push(<BsStarHalf key={i} size={13} className="text-black dark:text-white mr-0.5" />);
+    } else {
+      stars.push(<AiOutlineStar key={i} size={14} className="text-black dark:text-white opacity-30 mr-0.5" />);
+    }
   }
+
   return <div className="flex items-center">{stars}</div>;
 };
 
