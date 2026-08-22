7ffdddba28ee66a1c6c5e8ebc893a3272c33bc38
ntv2609 <ntv2609@gmail.com>
2026-08-22 23:59:24 +0700

    fix lỗi đăng ký thường bằng otp -> done. LƯU Ý: otp gửi về mail có thể nằm trong thư rác do không có chứng chỉ tin cậy của google

--- server/utils/sendMail.ts
+++ server/utils/sendMail.ts
@@ -11,22 +11,26 @@ interface EmailOptions {
 }
 
 const sendMail = async (options: EmailOptions): Promise<void> => {
+    const port = parseInt(process.env.SMTP_PORT || '587');
+    
     const transporter: Transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || '587'),  
+        port: port,  
+        // FIX LỖI PENDING: Tự động bật bảo mật SSL/TLS nếu dùng port 465
+        secure: port === 465, 
         service: process.env.SMTP_SERVICE,
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
         }
-    })
+    });
 
     const { email, subject, template, data } = options;
 
-    // get the path to the email template file
+    // Lấy đường dẫn tới file giao diện email
     const templatePath = path.join(__dirname, '../mails', template);
 
-    // render the email template with EJS
+    // Render email template với EJS
     const html:string = await ejs.renderFile(templatePath, data);
 
     const mailOptions = {
